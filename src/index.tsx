8db2eb6b781f7d2dd93a63bee5309e13e93c87e0
PraveenP <ppraveetr@gmail.com>
2024-06-20 21:44:14 -0400

    Updated build scripts

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import _ from 'lodash';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,4 @@
 import React  from 'react';
-import Container from 'react-bootstrap/Container';
-
 import Navigation from "./Navigation";
 import AboutUs from "./AboutUs"
 import Footer from "./Footer"
